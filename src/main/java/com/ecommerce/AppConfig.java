1b17bbeb49d152b2e63008a4a581d321b7b90f4f
S-Meyyappan <goatsforever0710@gmail.com>
2026-07-08 21:20:14 +0530

    Impl-Hikari-DataSource-JdbcTemplate-Using-ApplnProperties

--- src/main/java/com/ecommerce/AppConfig.java
+++ src/main/java/com/ecommerce/AppConfig.java
@@ -1,11 +1,43 @@
 package com.ecommerce;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:appln.properties")
 public class AppConfig {
-    static {
-        System.out.println("AppConfig loaded");
+
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+    @Value("${db.driver}")
+    private String driver;
+
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setDriverClassName(driver);
+
+        return new HikariDataSource(hikariConfig); // DataSource
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource){
+        System.out.println("DataSource "+ dataSource);
+        return new JdbcTemplate(dataSource);
     }
 
 }
--- src/main/java/com/ecommerce/MainApp.java
+++ src/main/java/com/ecommerce/MainApp.java
@@ -1,10 +1,13 @@
 package com.ecommerce;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class MainApp {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+        System.out.println("JdbcTemplate " + jdbcTemplate);
         context.close();
     }
 }
